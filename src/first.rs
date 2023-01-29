4a7281cfb0c76506d9163f2e61b30522783763e6
Sergei Kharchikov <callmestech@gmail.com>
2023-01-29 20:27:07 +0000

    Add push method for list. Add fmt check and clippy pipeline steps

--- src/first.rs
+++ src/first.rs
@@ -1,4 +1,28 @@
-pub enum List {
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+enum Link {
     Empty,
-    Elem(i32, Box<List>),
+    More(Box<Node>),
+}
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(new_node);
+    }
 }
