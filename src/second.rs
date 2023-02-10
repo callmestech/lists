d91007f26d2754bec95aac27b2800050256849fd
Sergei Kharchikov <callmestech@gmail.com>
2023-02-10 20:26:20 +0000

    3.4 IntoIter

--- src/second.rs
+++ src/second.rs
@@ -41,6 +41,10 @@ impl<T> List<T> {
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.elem)
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -55,6 +59,16 @@ impl<T> Drop for List<T> {
 impl<T> Default for List<T> {
     fn default() -> Self {
         Self { head: None }
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // accessing tuple struct numerically
+        self.0.pop()
     }
 }
 
@@ -106,5 +120,19 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
