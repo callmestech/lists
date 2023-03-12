dcb4ed2646d8c6aa8dc6896050afb34959a492af
Sergei Kharchikov <callmestech@gmail.com>
2023-03-12 22:52:13 +0000

    5.1, 5.2

---
+++ src/fourth.rs
@@ -0,0 +1,55 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem: elem,
+            next: None,
+            prev: None,
+        }))
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None,
+            tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links, everything else should be +0
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // non-empty list, need to connect the old_head
+                old_head.borrow_mut().prev = Some(new_head.clone()); // +1 new_head
+                new_head.borrow_mut().next = Some(old_head);         // +1 old_head
+                self.head = Some(new_head);             // +1 new_head, -1 old_head
+                // total: +2 new_head, +0 old_head -- OK!
+            }
+            None => {
+                // empty list, need to set the tail
+                self.tail = Some(new_head.clone());     // +1 new_head
+                self.head = Some(new_head);             // +1 new_head
+                // total: +2 new_head -- OK!
+            }
+        }
+    }
+    
+}
