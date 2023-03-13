5b48a8460f950126885b5db07630d7b5e8720d86
Sergei Kharchikov <callmestech@gmail.com>
2023-03-13 23:08:32 +0000

    5.3 wip

--- src/fourth.rs
+++ src/fourth.rs
@@ -39,17 +39,37 @@ impl<T> List<T> {
             Some(old_head) => {
                 // non-empty list, need to connect the old_head
                 old_head.borrow_mut().prev = Some(new_head.clone()); // +1 new_head
-                new_head.borrow_mut().next = Some(old_head);         // +1 old_head
-                self.head = Some(new_head);             // +1 new_head, -1 old_head
-                // total: +2 new_head, +0 old_head -- OK!
+                new_head.borrow_mut().next = Some(old_head); // +1 old_head
+                self.head = Some(new_head); // +1 new_head, -1 old_head
+                                            // total: +2 new_head, +0 old_head -- OK!
             }
             None => {
                 // empty list, need to set the tail
-                self.tail = Some(new_head.clone());     // +1 new_head
-                self.head = Some(new_head);             // +1 new_head
-                // total: +2 new_head -- OK!
+                self.tail = Some(new_head.clone()); // +1 new_head
+                self.head = Some(new_head); // +1 new_head
+                                            // total: +2 new_head -- OK!
             }
         }
     }
-    
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            // -1 old
+            match old_head.borrow_mut().next.take() {
+                Some(new_head) => {
+                    // -1 new
+                    // not emptying list
+                    new_head.borrow_mut().prev.take(); // -1 old
+                    self.head = Some(new_head); // +1 new
+                                                // total: -2 old, +0 new
+                }
+                None => {
+                    // emptying list
+                    self.tail.take(); // -1 old
+                                      // total: -2 old, (no new)
+                }
+            }
+            Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
+        })
+    }
 }
