a7a3caa5bf21eb5f9bc0e8e0f855aee671e303ca
Sergei Kharchikov <callmestech@gmail.com>
2023-04-07 19:14:36 +0100

    6.3 Basics

--- src/fifth.rs
+++ src/fifth.rs
@@ -41,48 +41,58 @@ impl<T> List<T> {
         self.tail = raw_tail;
     }
 
-    // pub fn pop(&mut self) -> Option<T> {
-    //     self.head.take().map(|head| {
-    //         let head = *head;
-    //         self.head = head.next;
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            let head = *head;
+            self.head = head.next;
 
-    //         if self.head.is_none() {
-    //             self.tail = None;
-    //         }
+            if self.head.is_none() {
+                self.tail = null_mut();
+            }
 
-    //         head.elem
-    //     })
-    // }
+            head.elem
+        })
+    }
 }
 
 #[cfg(test)]
 mod test {
-    // use super::List;
-    // #[test]
-    // fn basics() {
-    // let mut list = List::new();
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
 
-    // // Check empty list behaves right
-    // assert_eq!(list.pop(), None);
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
 
-    // // Populate list
-    // list.push(1);
-    // list.push(2);
-    // list.push(3);
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
-    // // Check normal removal
-    // assert_eq!(list.pop(), Some(1));
-    // assert_eq!(list.pop(), Some(2));
+        // Check normal removal
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(2));
 
-    // // Push some more just to make sure nothing's corrupted
-    // list.push(4);
-    // list.push(5);
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
 
-    // // Check normal removal
-    // assert_eq!(list.pop(), Some(4));
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(4));
 
-    // // Check exhaustion
-    // assert_eq!(list.pop(), Some(5));
-    // assert_eq!(list.pop(), None);
-    // }
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // Check the exhaustion case fixed the pointer right
+        list.push(6);
+        list.push(7);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(6));
+        assert_eq!(list.pop(), Some(7));
+        assert_eq!(list.pop(), None);
+    }
 }
